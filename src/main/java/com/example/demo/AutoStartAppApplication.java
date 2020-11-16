3a4910f371fb31ac49d7cdb172fe8137fee14779
Nagaraj S Kharvi <nagarajskharvi@Apples-MacBook-Air.local>
2020-11-16 23:58:10 +0530

    first commit

---
+++ src/main/java/com/example/demo/AutoStartAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoStartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoStartAppApplication.class, args);
+	}
+
+}

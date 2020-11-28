a8d0e269668909b70a0a5c28ca2a83a2220c7d51
Nagaraj S Kharvi <kharvinagaraj1@gmail.com>
2020-11-28 15:36:09 +0530

    Added Dockerfile

--- src/main/java/com/example/demo/example/Test.java
+++ src/main/java/com/example/demo/example/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 100; i++) {
-			System.out.println("Hello World Ping " + i );
+			System.out.println("Hello World Ping " + i);
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
